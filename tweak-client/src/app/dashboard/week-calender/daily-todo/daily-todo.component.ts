fdc9137b9999dea89b08b730234b327fb6d4384a
pkeweloh <skunk1@gmail.com>
2026-02-05 22:47:33 +0100

    Align calendar layout, styling, and interactions with tweek.so

--- tweak-client/src/app/dashboard/week-calender/daily-todo/daily-todo.component.ts
+++ tweak-client/src/app/dashboard/week-calender/daily-todo/daily-todo.component.ts
@@ -12,39 +12,63 @@ import { DialoagboxComponent } from './dialoagbox/dialoagbox.component';
 @Component({
   selector: 'app-daily-todo',
   template: `
-    <div
-      cdkDropList
-      [cdkDropListData]="works"
-      [cdkDropListConnectedTo]="generatedIds"
-      *ngFor="let work of works; let idx = index"
-      [attr.data-index]="idx"
-      class="flex-col w-full flex justify-start py-2 border-b hover:border-indigo-600"
-      [id]="getUniqueId(work)"
-    >
+    <div class="flex flex-col">
       <div
-        cdkDrag
-        class="schedule-div"
-        fxLayout="row"
-        fxLayoutAlign="space-between center"
+        cdkDropList
+        [cdkDropListData]="works"
+        [cdkDropListConnectedTo]="generatedIds"
+        [id]="getUniqueIdFromDate()"
+        (cdkDropListDropped)="onDropped($event)"
+        class="min-h-0"
       >
-        <div>
+        <div
+          *ngFor="let slot of slots; let idx = index"
+          class="flex-col w-full flex justify-start border-b h-[45px]"
+          [ngClass]="{
+            'hover:border-[#5167F466]': works[idx] || idx === works.length,
+            'cursor-pointer': works[idx],
+            'cursor-text': idx === works.length
+          }"
+        >
           <div
-            class="outline-none cursor-pointer border-none rounded-xl truncate inline px-2 w-full focus:bg-gray-50 focus:z-50"
+            *ngIf="works[idx] as work"
+            cdkDrag
+            [cdkDragData]="work"
+            class="schedule-div h-full flex items-center px-1 cursor-pointer"
+            fxLayout="row"
+            fxLayoutAlign="space-between center"
             (click)="launchDialog($event, editForms[idx])"
-            [ngClass]="
-              work.finished
-                ? ' opacity-50 line-through bg-gray-300'
-                : getColor(work.colorCode)
-            "
           >
-            {{ work.todo }}
+            <div class="flex-1 truncate">
+              <div
+                class="outline-none cursor-grab border-none rounded-xl truncate inline px-1.5 w-full focus:bg-gray-50 focus:z-50"
+[ngClass]="getColor(work.colorCode)"
+  [class.opacity-30]="work.finished"
+  [class.line-through]="work.finished"
+              >
+                {{ work.todo }}
+              </div>
+            </div>
+            <div class="display-on-parent-hover hidden ml-2">
+              <mat-checkbox
+                (click)="$event.stopPropagation()"
+                (change)="onCheck($event.checked, editForms[idx])"
+                [checked]="work.finished"
+              ></mat-checkbox>
+            </div>
           </div>
-        </div>
-        <div class="display-on-parent-hover hidden">
-          <mat-checkbox
-            (change)="onCheck($event.checked, editForms[idx])"
-            [checked]="work.finished"
-          ></mat-checkbox>
+
+          <!-- Empty Slot Placeholder / Add Form -->
+          <div
+            *ngIf="!works[idx]"
+            class="h-full w-full flex items-center text-gray-300 italic text-sm"
+          >
+            <app-add-form
+              *ngIf="idx === works.length"
+              [date]="date"
+              class="w-full h-full"
+            ></app-add-form>
+          </div>
         </div>
       </div>
     </div>
@@ -61,7 +85,6 @@ import { DialoagboxComponent } from './dialoagbox/dialoagbox.component';
       .bg-yellow-420 {
         background-color: #fdef5d;
       }
-
       .bg-blue-420 {
         background-color: #a3b1ff;
       }
@@ -72,8 +95,10 @@ export class DailyTodoComponent implements OnInit, OnDestroy {
   @Input() date!: Date;
   @Input() generatedIds!: Array<string>;
   @Input() connectedIndex!: number;
+  @Input() maxRows: number = 10;
 
   works: Array<Schedule> = [];
+  slots: Array<number> = [];
   editForms: Array<FormGroup> = [];
   bgColor: string = 'bg-transparent';
 
@@ -97,8 +122,21 @@ export class DailyTodoComponent implements OnInit, OnDestroy {
       this.works.forEach((work) =>
         this.editForms.push(this.createNewForm({ ...work }))
       );
+
+      this.generateSlots();
     });
   }
+
+  ngOnChanges(): void {
+    this.generateSlots();
+  }
+
+  private generateSlots() {
+    this.slots = Array(this.maxRows)
+      .fill(0)
+      .map((x, i) => i);
+  }
+
   ngOnDestroy(): void { }
 
   private createNewForm(work: Schedule): FormGroup {
@@ -118,14 +156,12 @@ export class DailyTodoComponent implements OnInit, OnDestroy {
   }
 
   onEdited(form: FormGroup) {
-    this.weeklyScheduleService
-      .updateSchedule({ ...form.value })
-      .subscribe(() => {
-        this.snackbar.open(`Schedule has been updated`, 'Done', {
-          duration: 3000,
-          panelClass: ['bg-indigo-700', 'text-white'],
-        });
+    this.weeklyScheduleService.updateSchedule({ ...form.value }).subscribe(() => {
+      this.snackbar.open(`Schedule has been updated`, 'Done', {
+        duration: 3000,
+        panelClass: ['bg-[#5167F4]', 'text-white'],
       });
+    });
   }
 
   launchDialog(_: MouseEvent, form: FormGroup) {
@@ -136,6 +172,7 @@ export class DailyTodoComponent implements OnInit, OnDestroy {
     });
 
     dialogRef.afterClosed().subscribe((result) => {
+      if (!result) return;
       form.setValue({ ...result });
       if (JSON.stringify(previousState) === JSON.stringify({ ...form.value }))
         return;
@@ -147,19 +184,16 @@ export class DailyTodoComponent implements OnInit, OnDestroy {
     return ColorUtils.COLORS[+colorCode];
   }
 
-  onDropped(event: CdkDragDrop<Array<Schedule>>) {
-    this.dragDropService.drop(event);
-    // const previousContainerId = event.previousContainer.id;
-    // const currentContainerId = event.container.id;
-
-    // const oldDate = previousContainerId.split('@')[2];
-    // const curDate = currentContainerId.split('@')[2];
-
-    // console.log({ oldDate, curDate });
-  }
-
   onCheck(state: boolean, form: FormGroup) {
     form.setValue({ ...form.value, finished: state });
     this.onEdited(form);
+  }
+
+  onDropped(event: CdkDragDrop<Array<Schedule>>) {
+    this.dragDropService.drop(event);
+  }
+
+  getUniqueIdFromDate() {
+    return `ID@${this.date.toDateString()}`;
   }
 }
--- tweak-client/src/app/shared/utils/colors.utils.ts
+++ tweak-client/src/app/shared/utils/colors.utils.ts
@@ -1,9 +1,12 @@
 export class ColorUtils {
   static readonly COLORS: Array<string> = [
     'bg-transparent',
+    'bg-red-700 text-white',
     'bg-yellow-420',
+    'bg-fuchsia-500',
     'bg-green-420',
-    'bg-blue-420',
-    'bg-red-500',
+    'bg-amber-400',
+    'bg-indigo-500 text-white',
+    'bg-black text-white',
   ];
 }
